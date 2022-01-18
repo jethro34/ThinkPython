f011bd3e9a9f93114ad9dafb26ea757b5fb851b3
jethro34 <jh.triana@hotmail.com>
2022-01-17 21:45:14 -0500

    v.1

---
+++ p78ex_rewrite_function.py
@@ -0,0 +1,17 @@
+# def(print_n(string, n):
+#     """Prints a string n times by recursion"""
+#     if n <= 0:
+#         return
+#     print(string)
+#     print_n(string, n - 1)
+
+# the exercise asks the above recursive function to be changed into an iterative one:
+
+def print_n(string, n):
+    """Prints a string n times by iteration"""
+    while n > 0:
+        print(string)
+        n -= 1
+
+
+print_n("bitch", 3)
