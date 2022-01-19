b5d4397167a3d3ec406e5c0be1e839cc78352f9c
jethro34 <77173690+jethro34@users.noreply.github.com>
2022-01-19 01:49:36 +0000

    v.2

--- p46ex4-4_alphabet.py
+++ p46ex4-4_alphabet.py
@@ -1,6 +1,7 @@
 # draws alphabet letters
 
 import turtle
+import math
 
 coord_key = {'1': (-10, -20), '2': (0, -20), '3': (10, -20), '4': (-10, 0),
              '5': (0, 0), '6': (10, 0), '7': (-10, 20), '8': (0, 20), '9': (10, 20)}
@@ -10,7 +11,6 @@ def rotate_n_draw(t, orig_coord, dest_coord):
     t.setheading(to_angle)
     distance = math.sqrt((dest_coord[1] - orig_coord[1]) ** 2 + (dest_coord[0] - orig_coord[0]) ** 2)
     t.forward(distance)
-    return
 
 
 def print_letter(t, string_code):
@@ -18,20 +18,14 @@ def print_letter(t, string_code):
     count = 0 
     for ch in string_code[:-1]:
         count += 1
-        if ch == '0': #or string_code[1] == '0': TO BE FIXED
+        if ch == '0' or string_code[count] == '0':
             continue
         t.pendown()
-        rotate_n_draw(t, coordinates(ch), coordinates(string_code[ch+1]))
+        rotate_n_draw(t, coord_key[ch], coord_key[string_code[count]])
     t.penup()
 
 
-def something():
-    return
-
-
 pointer = turtle.Turtle()
-print(pointer)
+#print(pointer)
 print_letter(pointer, "1834")
-
-        
-        
+pointer.hideturtle()
