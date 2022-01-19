41f6c8a0f4a1ce3ed1568bc28064a0630e6e958b
jethro34 <77173690+jethro34@users.noreply.github.com>
2022-01-19 00:21:37 +0000

    v.1.1

--- p46ex4-4_alphabet.py
+++ p46ex4-4_alphabet.py
@@ -2,6 +2,9 @@
 
 import turtle
 
+coord_key = {'1': (-10, -20), '2': (0, -20), '3': (10, -20), '4': (-10, 0),
+             '5': (0, 0), '6': (10, 0), '7': (-10, 20), '8': (0, 20), '9': (10, 20)}
+
 def rotate_n_draw(t, orig_coord, dest_coord):
     to_angle = t.towards(dest_coord)
     t.setheading(to_angle)
@@ -11,9 +14,11 @@ def rotate_n_draw(t, orig_coord, dest_coord):
 
 
 def print_letter(t, string_code):
-    pointer.goto(coordinates(string_code[0]))         # setting pen's initial location 
+    pointer.goto(coord_key[string_code[0]])         # setting pen's initial location
+    count = 0 
     for ch in string_code[:-1]:
-        if ch == '0' or string_code[ch+1] == '0':
+        count += 1
+        if ch == '0': #or string_code[1] == '0': TO BE FIXED
             continue
         t.pendown()
         rotate_n_draw(t, coordinates(ch), coordinates(string_code[ch+1]))
