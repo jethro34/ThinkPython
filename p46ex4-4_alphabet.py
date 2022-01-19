babf2be20f5546ae4aba7e819b47be37bb9bafba
jethro34 <77173690+jethro34@users.noreply.github.com>
2022-01-19 00:01:30 +0000

    v.1.1

--- p46ex4-4_alphabet.py
+++ p46ex4-4_alphabet.py
@@ -2,19 +2,22 @@
 
 import turtle
 
-def rotate(t, orig_coord, dest_coord):
+def rotate_n_draw(t, orig_coord, dest_coord):
+    to_angle = t.towards(dest_coord)
+    t.setheading(to_angle)
+    distance = math.sqrt((dest_coord[1] - orig_coord[1]) ** 2 + (dest_coord[0] - orig_coord[0]) ** 2)
+    t.forward(distance)
     return
 
 
 def print_letter(t, string_code):
-    pointer.goto(coordinates(string_code[1]))                            # setting pen's initial location 
+    pointer.goto(coordinates(string_code[0]))         # setting pen's initial location 
     for ch in string_code[:-1]:
         if ch == '0' or string_code[ch+1] == '0':
             continue
         t.pendown()
-        #rotate(coordinates(ch), coordinates(string_code[ch+1]))
-        #draw_line(coordinates(ch), coordingates(string_code[ch+1]))
-    #pen.up
+        rotate_n_draw(t, coordinates(ch), coordinates(string_code[ch+1]))
+    t.penup()
 
 
 def something():
@@ -23,7 +26,7 @@ def something():
 
 pointer = turtle.Turtle()
 print(pointer)
-print_letter("1834")
+print_letter(pointer, "1834")
 
         
         
