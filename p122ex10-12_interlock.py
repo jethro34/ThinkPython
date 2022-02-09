18e0f77a593609cb74d00efba5a1cafe142a0efd
jethro34 <jh.triana@hotmail.com>
2022-02-08 23:20:30 -0500

    v.2 (optimized)

--- p122ex10-12_interlock.py
+++ p122ex10-12_interlock.py
@@ -1,4 +1,4 @@
-def created_list():
+def create_list():
     """Creates a list by reading from a file."""
     w_list = []
     fin = open("/Users/hejtor/OneDrive/CS/ThinkPython stuff/words.txt")
@@ -8,49 +8,42 @@ def created_list():
 
 
 def in_bisect(l, target):
-    """Returns the index of target in a given list l, using bi-section."""
+    """Returns True if target is in a given list, False otherwise, using bi-section."""
     mini = 0
     maxi = len(l) - 1
     if l[mini] <= target <= l[maxi]:
         while mini <= maxi:
             indeks = mini + (maxi - mini) // 2
             if l[indeks] == target:
-                return indeks
+                return True
             elif l[indeks] > target:
                 maxi = indeks - 1
             else:
                 mini = indeks + 1
-    return 'not in range'
+    return False
 
 
-def interlocks(w1, w2):
-    """Returns a tuple with interlocks from two given words."""
-    temp = ''
-    temp_rev = ''
-    for ch1, ch2 in zip(w1, w2):
-        temp += ch1 + ch2
-        temp_rev += ch2 + ch1
-    return temp, temp_rev
+def interlock(big_w):
+    """Returns a tuple with three interlocked words from a given word."""
+    w1 = big_w[::3]
+    w2 = big_w[1::3]
+    w3 = big_w[2::3]
+    return w1, w2, w3
 
 
-def find_w2_n_check(l, w1):
-    """Checks interlocks from a given word and the next same-length word in a given list."""
-    global hits
-    for w2_indeks in range(l.index(w1), len(l)):
-        if len(w1) == len(l[w2_indeks]):
-            if in_bisect(l, interlocks(w1, l[w2_indeks])[0]) != 'not in range':
-                print(w1, l[w2_indeks], l[in_bisect(l, interlocks(w1, l[w2_indeks])[0])])
-                hits += 1
-            elif in_bisect(l, interlocks(w1, l[w2_indeks])[1]) != 'not in range':
-                print(w1, l[w2_indeks], l[in_bisect(l, interlocks(w1, l[w2_indeks])[1])])
-                hits += 1
+def check(l, w1, w2, w3):
+    """Checks interlocks from a given word."""
+    global hits, word
+
+    if in_bisect(l, w1) and in_bisect(l, w2) and in_bisect(l, w3):
+        print(word, w1, w2, w3)
+        hits += 1
 
 
-liste = created_list()
+liste = create_list()
 hits = 0
 
 for word in liste:
-    if len(word) < 4:
-        find_w2_n_check(liste, word)
+    check(liste, interlock(word)[0], interlock(word)[1], interlock(word)[2])
 
 print('there were', hits, 'hits!')
