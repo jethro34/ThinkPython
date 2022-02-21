8c397759eea223a3eabcf7fa2df2dc2cafb406be
jethro34 <jh.triana@hotmail.com>
2022-02-21 09:41:09 -0500

    changed counter to sum(dikt.values())

--- p153ex13-5_choose_from_hist.py
+++ p153ex13-5_choose_from_hist.py
@@ -17,13 +17,11 @@ def zeichen_im_wort(string):
     """ Takes a string and returns a histogram dictionary and a total of its values.
         Output: tuple of a dictionary and an integer """
 
-    zahler = 0
     dikt = {}
     for zeichen in string:
         dikt[zeichen] = dikt.get(zeichen, 0) + 1
-        zahler += 1
 
-    return dikt, zahler
+    return dikt, sum(dikt.values())
 
 
 d = zeichen_im_wort('eresputamuyputaputissima')
